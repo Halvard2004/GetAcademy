87dd8ce67642ffdd95ca706e2dc88b7b80a1c877
Halvard2004 <halvardgjeisar@gmail.com>
2025-01-24 12:34:20 +0100

    24.01

---
+++ Emne1/Intro/Oppgaver/Javascript.js
@@ -0,0 +1,9 @@
+function Addition(){
+    document.getElementById("Number").innerHTML = 1 + Number(document.getElementById("Number").innerHTML)
+
+}
+
+function Multiplication(){
+    document.getElementById("Number").innerHTML = 2 * Number(document.getElementById("Number").innerHTML)
+
+}
