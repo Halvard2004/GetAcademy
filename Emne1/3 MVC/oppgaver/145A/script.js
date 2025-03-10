6de023ccc81b0362541973bf29cb2ac9b4abd2ac
Halvard2004 <halvardgjeisar@gmail.com>
2025-03-10 13:54:29 +0100

    Updated

--- Emne1/3 MVC/oppgaver/145A/script.js
+++ Emne1/3 MVC/oppgaver/145A/script.js
@@ -1,5 +1,6 @@
 /* Model  */
-let squares = 4;
+let squares = [4, 9, 16, 25, 36];
+let squareAmountSelect = 4;
 let SelectedSquare = 0;
 
 /* View */
@@ -7,21 +8,63 @@ updateView();
 function updateView(){
 
     document.getElementById("app").innerHTML = /*HTML*/ `
-    ${createSquareHtml()}
+    <div id="container" style="width: ${widthChoice("mainBox")}px">${createSquareHtml()}</div>
+    <div id="buttonContainer" style="width: ${widthChoice("mainBox")}px">${createButtonHtml()}</div>
     `;
 }
 
 function createSquareHtml(){
-let HTMLdivs;
-    HTMLdivs = `<div id="container">`
-    for(let i = 0; i < squares; i++){
-        HTMLdivs += `<div id="Box${i}">${Selected(i) ? "Hello" : ""}</div>`
+let HTMLdivs = "";
+    for(let i = 0; i < squares[squareAmountSelect]; i++){
+        HTMLdivs += `<div id="Box" style="width: ${widthChoice("Box")}px">${Selected(i) ? "Hello" : ""}</div>`
     }
-    HTMLdivs += "</div>"
     return HTMLdivs
 }
 
+function createButtonHtml(){
+    let buttons = ["Up", "Left", "Down", "Right"]
+    let HTMLButtons = "";
+        for (let button of buttons){
+            HTMLButtons += /*HTML*/ `<button onclick="changeSelected('${button}')">${button}</button>`;
+        }
+         return HTMLButtons;
+}
+
 /* Controller */
-function Selected(){
-    
+function Selected(square){
+    for(let i = 0; i < squares[squareAmountSelect]; i++){
+        if(square == i && SelectedSquare == i){
+            return true
+        }
+    }
+}
+
+function changeSelected(movementButton){
+    const movement = convertButtonMovement(movementButton);
+    SelectedSquare += movement;
+    updateView();
+}
+
+
+function convertButtonMovement(buttonVersion){
+    switch(buttonVersion){
+        case "Up":
+            return -Math.sqrt(squares[squareAmountSelect])
+        case "Left":
+            return -1
+        case "Down":
+            return Math.sqrt(squares[squareAmountSelect])
+        case "Right":
+            return 1
+    }
+}
+
+function widthChoice(idType){
+    if(idType == "mainBox"){
+        let lineSquares = Math.sqrt(squares[squareAmountSelect]);
+        widthSize = lineSquares * 200
+    } else if(idType == "Box"){
+        widthSize = 197
+    }
+    return widthSize
 }
